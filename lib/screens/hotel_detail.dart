8ba8f876101d81f6498ef3954b61e0857912009a
boluwatife <ogunfowokansherrfideen@gmail.com>
2024-10-24 15:36:26 +0100

    Hotel details

--- lib/screens/hotel_detail.dart
+++ lib/screens/hotel_detail.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/utils/app_json.dart';
 
 class HotelDetail extends StatefulWidget {
   const HotelDetail({super.key});
 
-  
   @override
   State<HotelDetail> createState() => _HotelDetailState();
 }
 
 class _HotelDetailState extends State<HotelDetail> {
-
-  late int hotelIndex=0;
+  late int hotelIndex = 0;
 
   @override
   void didChangeDependencies() {
-    if(ModalRoute.of(context)!.settings.arguments!=null){
-      var args =ModalRoute.of(context)!.settings.arguments as Map;
+    if (ModalRoute.of(context)!.settings.arguments != null) {
+      var args = ModalRoute.of(context)!.settings.arguments as Map;
       hotelIndex = args["index"];
     }
     super.didChangeDependencies();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,14 +29,56 @@ class _HotelDetailState extends State<HotelDetail> {
           SliverAppBar(
             expandedHeight: 300.0,
             floating: false,
+            pinned: true,
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: AppStyles.primaryColor),
+                  child: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
             flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset("assets/images/${hotelList[hotelIndex]['image']}"),
-              title: Text(hotelList[hotelIndex]["place"]) ,
+              background: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Image.asset(
+                      "assets/images/${hotelList[hotelIndex]['image']}",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child:  Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 4),
+                      color: Colors.black.withOpacity(0.5),
+                      child: Text(
+                      hotelList[hotelIndex]["place"],
+                      style:  TextStyle(color: Colors.white,fontSize: 24,shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: AppStyles.primaryColor,
+                          offset: Offset(2.0, 2.0)
+                        )
+                      ]),
+                                        ),
+                    ),)
+                ],
+              ),
             ),
           ),
           SliverList(
               delegate: SliverChildListDelegate([
-             Padding(
+            Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(hotelList[hotelIndex]["destination"]),
             ),
@@ -50,12 +91,13 @@ class _HotelDetailState extends State<HotelDetail> {
             Container(
               height: 200.0,
               child: ListView.builder(
-                scrollDirection: Axis.horizontal,
+                  scrollDirection: Axis.horizontal,
                   itemCount: 10,
                   itemBuilder: (context, index) {
                     return Container(
                         margin: EdgeInsets.all(8),
-                        child: Image.asset("assets/images/${hotelList[hotelIndex]['image']}"));
+                        child: Image.asset(
+                            "assets/images/${hotelList[hotelIndex]['image']}"));
                   }),
             )
           ]))
